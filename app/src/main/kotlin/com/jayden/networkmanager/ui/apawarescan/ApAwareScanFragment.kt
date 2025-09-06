35d8c4a95bb1850cf915d5efbda03bd414765af8
Jundertag <jaydenmacarthur3@gmail.com>
2025-09-06 18:37:07 -0300

    Adding Ap Aware Fragment with temp TextView

---
+++ app/src/main/kotlin/com/jayden/networkmanager/ui/apawarescan/ApAwareScanFragment.kt
@@ -0,0 +1,8 @@
+package com.jayden.networkmanager.ui.apawarescan
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ApAwareScanFragment : Fragment() {
+}
