f4f302e1d767a50f06a4fe1dd16236cacbf34039
Jundertag <jaydenmacarthur3@gmail.com>
2025-09-06 18:22:44 -0300

    My brain really wanted to start over 🙃

--- app/src/main/kotlin/com/jayden/networkmanager/ui/apscan/ApScanFragment.kt
+++ app/src/main/kotlin/com/jayden/networkmanager/ui/apscan/ApScanFragment.kt
@@ -1,9 +1,7 @@
 package com.jayden.networkmanager.ui.apscan
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jayden.networkmanager.R
 import com.jayden.networkmanager.databinding.FragmentScanBinding
